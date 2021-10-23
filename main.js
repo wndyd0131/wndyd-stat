db58eb7ed28723dbb18e21d68082a07cf5922159
Muruky <qpsvs0131@gmail.com>
2021-10-23 13:51:16 +0900

    newer_commit

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 //console.log(process.argv); //argument vector
 const lib = require("./lib");
 if (process.argv.length <= 3){
-  console.log("Insufficient parameter!"); //ex) node main.js sum�� �ڿ� ���ڵ��� �Է����� �ʾƼ� 3���� ����
+  console.log("Insufficient parameter!"); 
   process.exit(1);
 }
 
@@ -11,8 +11,8 @@ let command = process.argv[2];
 
 let numbers = process.argv.slice(3, process.argv.length).map((n) => parseFloat(n));
 
-if(numbers.some((n) => isNaN(n))){ //n == any: any �߿��� �ϳ��� NaN�̸�
-  console.log("Some arguments are nott numbers!");
+if(numbers.some((n) => isNaN(n))){
+  console.log("Some arguments are not numbers!");
   process.exit(1);
 }
 
@@ -27,13 +27,25 @@ switch(command){
   case "max":
     result = lib.max(numbers);
     break;
-  case "mean2":
+  case "mean2": //error handling: must exist at least 1 for each, else exit
+    if(numbers.length % 2 !== 0){
+      console.log("Number of the input must be even to use this function!");
+      process.exit(1);
+    }
     result = lib.mean2(numbers);
     break;
   case "med2":
+    if(numbers.length % 2 !== 0){
+      console.log("Number of the input must be even to use this function!");
+      process.exit(1);
+    }
     result = lib.med2(numbers);
     break;
   case "pareto":
+    if(numbers.length % 2 !== 0){
+      console.log("Number of the input must be even to use this function!");
+      process.exit(1);
+    }
     result = lib.pareto(numbers);
     break;
   default:
